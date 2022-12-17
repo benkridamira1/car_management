d3bb8696d5b6997b3420b5ef200342ea9f3365bf
Amira <benkridamira14@gmail.com>
2022-12-17 22:36:42 +0100

    add car model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,29 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_title=models.CharField(max_length=255)
+    city=models.CharField(max_length=100)
+    color=models.CharField(max_length=100)
+    model=models.CharField(max_length=100)
+    year=models.IntegerField()
+    condition=models.CharField(max_length=100)
+    price=models.IntegerField()
+    description=models.TextField(max_length=500)
+    car_photo=models.ImageField(upload_to='photos/%Y/%m/%d/')
+    car_photo_1=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_2=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_3=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_4=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    body_style=models.CharField(max_length=100)
+    engine=models.CharField(max_length=100)
+    transmission=models.CharField(max_length=100)
+    interior=models.CharField(max_length=100)
+    miles=models.IntegerField()
+    doors=models.IntegerField()
+    passengers=models.IntegerField()
+    milage=models.IntegerField()
+    fuel_type=models.CharField(max_length=100)
+    created_date=models.DateTimeField(auto_now_add=True)
+    
