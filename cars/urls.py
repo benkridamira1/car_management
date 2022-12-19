e49b92aaa9340f8c301063c33e38a1a67cfb8a66
Amira <benkridamira14@gmail.com>
2022-12-19 16:00:57 +0100

    pagination and search bar

--- cars/urls.py
+++ cars/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('',views.cars, name='cars'),
     path('<int:id>',views.car_detail, name='car_detail'),
+    path('/search',views.search, name='search'),
 ]
